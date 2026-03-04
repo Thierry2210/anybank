d630a373f2ff33631b05b3a0357397a0037ae689
Thierry <matheusthierry@gmail.com>
2026-03-04 18:31:08 -0300

    Adicionando metódo privado

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
+import 'conta.dart';
 
 void main() {
-  Conta contaMatheus = Conta ("Matheus", 1000);
-  Conta contaRoberta = Conta ("Roberta", 1000);
-  List<Conta> contas = <Conta> [contaMatheus, contaRoberta];
+  Conta contaMatheus = Conta("Matheus", 1000);
+  Conta contaRoberta = Conta("Roberta", 1000);
+  List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
 
-  for(Conta conta in contas) {
-    print(conta.titular);
-    print(conta.saldo);
+  for (Conta conta in contas) {
+    conta.imprimeSaldo();
   }
-}
+  contaRoberta.receber(3000);
 
-class Conta {
-  String titular;
-  double saldo;
+  contaMatheus.receber(1000);
 
-  Conta(this.titular, this.saldo);
+  contaMatheus.enviar(500);
 }
