4489cdfcaa797c601a51a1b0380aff26238e744b
Seu Nome <seu@email.com>
2026-03-10 19:11:09 -0300

    Herença e Polimorfismo

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,25 @@ import 'conta.dart';
 
 void main() {
   Conta contaMatheus = Conta("Matheus", 1000);
-  Conta contaRoberta = Conta("Roberta", 1000);
+  Conta contaRoberta = Conta("Roberta", 2000);
+  ContaCorrente contaChris = ContaCorrente("Chris", 4000);
+  ContaPoupanca contaDenize = ContaPoupanca("Denize", 4000);
+
   List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
 
   for (Conta conta in contas) {
     conta.imprimeSaldo();
   }
+
   contaRoberta.receber(3000);
-
   contaMatheus.receber(1000);
-
   contaMatheus.enviar(500);
+
+  contaChris.imprimeSaldo();
+  contaChris.enviar(4300);
+  
+  contaDenize.imprimeSaldo();
+  contaDenize.enviar(4300);
+  contaDenize.calculaRendimento();
+  contaDenize.imprimeSaldo();
 }
